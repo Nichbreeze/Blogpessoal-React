8b0db6a918b245b1cdb1cd092d36697528c673a5
Nich breeze <nicholas.cavalcante05@gmail.com>
2023-03-13 09:18:12 -0300

    redux

--- React/src/Component/Estaticos/footer/Footer.tsx
+++ React/src/Component/Estaticos/footer/Footer.tsx
@@ -1,15 +1,57 @@
-import { Grid } from "@material-ui/core";
+import React from "react";
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GithubIcon from '@material-ui/icons/GitHub';
+import { Grid, Typography } from "@material-ui/core";
+import { Box } from "@mui/material";
 import './Footer.css';
+import { useSelector } from "react-redux";
+import { TokenState } from "../../../store/tokens/tokensReducer";
+
+
+
 function Footer(){
-    return(
-        <>
-            <Grid container spacing={0} className="footer--container">
-                <Grid item xs={12} alignItems="center" className="footer--item">
-                    <span>Desenvolvido por Nick</span>
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+      );
+
+      var footerComponent;
+      if(token != ""){
+       footerComponent = <Grid container  direction="row" justifyContent="center" alignItems="center">
+                <Grid alignItems="center" item xs={12}>
+                    <Box className='box1'>
+                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
+                            <Typography variant="h5" align="center" gutterBottom className='textos'>Follow me: </Typography>
+                        </Box>
+                        <Box display="flex" alignItems="center" justifyContent="center">
+                        < href="https:/http://github.com/Nichbreeze" target="_blank">
+                                <GithubIcon className='redes'/>
+                
+                            <a href="https://www.instagram.com/nickbreeze" target="_blank">
+                                <InstagramIcon className='redes'/>
+                            </a>
+                            <a href="https://www.linkedin.com/in/nicholas-cavalcante-%F0%9F%8F%B3%EF%B8%8F%E2%80%8D%F0%9F%8C%88-4b76951b5/" target="_blank">
+                                <LinkedInIcon className='redes'/>
+                            </a>
+                        </Box>
+                    </Box>
+                    <Box className='box2' >
+                        <Box paddingTop={1}>
+                            <Typography variant="subtitle2" align="center" gutterBottom className='textos' >© 2023 Copyright:</Typography>
+                        </Box>
+                        <Box >
+                      
+                        </Box>
+                    </Box>
                 </Grid>
             </Grid>
-        </>
-    );
-}
+      }
+    return(
+        <>
+        {footerComponent}
+    </>
 
-export default Footer
+    )
+}
+export default Footer;
--- React/src/Component/Estaticos/nav/Nav.tsx
+++ React/src/Component/Estaticos/nav/Nav.tsx
@@ -1,36 +1,105 @@
 import React from "react";
-import {
-  AppBar,
-  Button,
-  IconButton,
-  Toolbar,
-  Typography,
-} from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
+import { AppBar, Toolbar, Typography} from '@material-ui/core';
+import { Box } from '@mui/material';
+import { FaDev } from 'react-icons/fa';
+import { Link } from "react-router-dom";
+import {useNavigate} from 'react-router-dom'
+import useLocalStorage from "react-use-localstorage";
+import './Navbar.css';
+import { useDispatch, useSelector } from "react-redux";
+import { TokenState } from "../../../store/tokens/tokensReducer";
+import { addToken } from "../../../store/tokens/actions";
+import {toast} from 'react-toastify';
 
-function Navbar() {
-  return (
-    <>
-      <div className="nav--container">
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              className="menuButton"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className="title">
-              News
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </div>
+function Navbar(){
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+      );
+
+    let navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    function goLogout(){
+        dispatch(addToken(''));
+        toast.info('Usuário deslogado', {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            theme: "colored",
+            progress: undefined,
+        });
+        navigate('/login')
+        navigate('/login')
+    }
+    var navbarComponent;
+    if(token != ""){
+        navbarComponent = <AppBar  position="static" className="backcolor">
+                <Toolbar variant="dense">
+                    <Box className='cursor'>
+                        <Typography  variant="h5" color="inherit">
+                        <Link to='/home' className='text-decorator-none'>
+                        <FaDev className="dev"/> <b className="dev">Java Full Stack Jr.</b>
+                        </Link>
+                        </Typography>
+                    </Box>
+
+                    <Box display="flex" justifyContent="start">
+                        <Link to="/home" className="text-decoration">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                 | Home |
+                            </Typography>
+                        </Box>
+                        </Link>
+                        <Link className="text-decoration" to="/postagens">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit" className="text-decoration" >
+                               | Postagens |
+                            </Typography>
+                        </Box>
+                        </Link>
+                        <Link to="/tema" className="text-decoration">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                               | Tema |
+                            </Typography>
+                        </Box>
+                        </Link>
+                        <Link to="/formularioTema" className="text-decoration">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                               | Cadast. Temas |
+                            </Typography>
+                        </Box>
+                        </Link>
+                        <Link to="/portfolio" className="text-decoration">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                               | Portfólio |
+                            </Typography>
+                        </Box>
+                        </Link>
+
+        
+                            <Box mx={1} className='cursor' onClick={goLogout}>
+                                <Typography variant="h6" color="inherit">
+                                <b className="text-decoraion">Sair</b>
+                                </Typography>
+                            </Box>
+                        
+                    </Box>
+
+                </Toolbar>
+            </AppBar>
+    }
+    return(
+        <>
+        {navbarComponent}
     </>
-  );
-}
 
+    )
+}
 export default Navbar;
